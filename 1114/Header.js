2cfe8e26eb20d0cb4ba85d09b628bd46d5ee2ff9
Zac <a19860101@gmail.com>
2020-11-14 15:42:16 +0800

    20201114

---
+++ 1114/Header.js
@@ -0,0 +1,5 @@
+app.component('my-header',{
+    template:`
+        <img src="https://picsum.photos/id/34/300"
+    `
+})
---
+++ 1114/Nav.js
@@ -0,0 +1,11 @@
+app.component('my-nav',{
+    template:`
+        <nav>
+            <a href="#">About</a>
+            <a href="#">Service</a>
+            <a href="#">Product</a>
+            <a href="#">Shop</a>
+            <a href="#">Contact</a>
+        </nav>
+    `
+})
