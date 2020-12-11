807fda0cc748382f05c2e5d9676c1c41161434e0
Zac <a19860101@gmail.com>
2020-12-11 20:53:14 +0800

    20201211

---
+++ 1211/index.js
@@ -0,0 +1,10 @@
+import {x} from './test.js';
+import {y} from './test.js';
+import {test} from './test.js';
+
+import HI from './test.js';
+console.log(HI);
+
+// console.log(x);
+// console.log(y);
+// test();
---
+++ 1211/test.js
@@ -0,0 +1,14 @@
+export const x = 100;
+export const y = 99;
+export function test(){
+    alert('hello module~!!');
+}
+const text ='hello world';
+// export default text;
+// export default function(){
+//     alert('test');
+// }
+export default {
+    name:'test',
+    mail: 'asdf@gmail.com'
+}
